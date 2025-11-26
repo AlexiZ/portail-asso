4f891db6106df4b0f4166be126bc0025638e8aa2
Alexis <mazaudiera@gmail.com>
2025-11-26 10:18:27 +0100

    fix(deprecated): using Attribute/Route instead of Annotation/Route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
